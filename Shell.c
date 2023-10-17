5da79134f300573599b93dacdb7b319d1029f0d4
ValxLyn <65049888+ValxLyn@users.noreply.github.com>
2023-10-16 21:22:01 -0600

    Update Shell.c

--- Shell.c
+++ Shell.c
@@ -1,3 +1,23 @@
+/**
+* Name(s): 		Andy Wang, Micheal Myer, Vincent Ha
+??????* Emails: 		vha117@mtroyal.ca
+* Course: 		COMP 3659 - 001
+* Instructor: 	Marc Schroeder
+* Assignment: 	Assignment 01
+* Source File: 	Shell.c
+* Due Date: 	October 6, 2023
+*
+* Purpose:		A program that emulates the Linux shell
+*
+* Details:		Will prompt user for commands and upon recieving a valid common execute
+*               the corresponding instruction. This will continue until exit command is
+*               entered which will then cause the program to terminate.
+*
+*               Any invalid commands will print out a error message.
+*
+* Assumptions and Limitations: 
+*              All valid user input will be specify the file path of the command
+*/
 #include "Shell.h"
 
 int main()
@@ -30,12 +50,36 @@ int main()
     return 0;
 }
 
+/*       
+    Function: readCommandLine
+    
+	Purpose: prompt user then call readInput to read user message
+	
+	Input:
+	
+	Return: pointer to where user input is stored
+
+    Assumptions and Limitations:
+*/
 int readCommandLine(char *commandLineStr)
 {
     write(1, promptMsg, mystrlen(promptMsg));
     return readInput(commandLineStr);
 }
 
+
+/*          
+    Function: readInput
+    
+	Purpose: read user message return succesful or failed read
+	
+	Input: buffer space to hold input
+	
+???	Return: 1 on succesful read
+            0 otherwise
+            
+    Assumptions and Limitations:
+*/
 int readInput(char *buffer)
 {
     if (read(0, buffer, MAX_BUFFER_LEN) == MAX_BUFFER_LEN && buffer[MAX_BUFFER_LEN - 1] != '\n')
@@ -47,6 +91,17 @@ int readInput(char *buffer)
     return 1;
 }
 
+/*          
+    Function flushInput
+    
+	Purpose: empty out buffer
+	
+	Input: buffer to empty
+	
+	Return:
+            
+    Assumptions and Limitations:
+*/
 void flushInput(char *buffer)
 {
     while (read(0, buffer, MAX_BUFFER_LEN) >= MAX_BUFFER_LEN)
